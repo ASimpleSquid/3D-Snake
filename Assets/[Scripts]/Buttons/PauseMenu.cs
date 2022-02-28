0908157fde1418e0c88cee955e8a17a8f9a40465
Justin Dela Cruz <Traxiliongames@gmail.com>
2022-02-27 19:24:38 -0500

    Pause Fully Functional, Running into Issues with Snake input

---
+++ Assets/[Scripts]/Buttons/PauseMenu.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] GameObject pauseMenu;
+
+    public void OnPauseButtonPressed()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+    }
+}
